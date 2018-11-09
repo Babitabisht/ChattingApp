f635e49b78d87f2cf56ff59cb449a926365a20f2
Babita Bisht <babitabisht2018@gmail.com>
2018-11-09 11:34:27 +0530

    user functionality added

--- server.js
+++ server.js
@@ -27,9 +27,40 @@ console.log('connected  : %s sockets connected',connections.length);
 
 socket.on('disconnect' ,(data)=>{
 
+
+    //if(!socket.username) return ;
+    users.splice(users.indexOf(socket.user),1)
+updateUsernames();
+
     connections.splice(connections.indexOf(socket),1)
     console.log('disconnected : %s sockets connected' ,connections.length);
 
 })
+
+
+//send message
+socket.on('send message',(data)=>{
+console.log(data)
+    io.sockets.emit('new message',{msg:data}  );
+   // $message.val('');
+})
+
+
+socket.on('new user'  ,(data , callback)=>{
+    callback(true);
+console.log('in server '+data)
+    socket.user=data;
+
+    users.push(socket.user);
+
+    updateUsernames();
+})
+
+
+
+function  updateUsernames(){
+io.sockets.emit('get users'  , users)
+
+}
 
 })
